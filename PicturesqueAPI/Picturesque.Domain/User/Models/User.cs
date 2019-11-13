371fad3bb5065a121fa51146cd66ae5a953b4d1d
Lusian Manolov <lusianmanolov@outlook.com>
2019-11-13 19:58:13 +0200

    Added User entity

---
+++ PicturesqueAPI/Picturesque.Domain/User/Models/User.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Picturesque.Domain
+{
+    public sealed class User
+    {
+        public User() { }
+
+        public User(
+            string email,
+            string username,
+            string password
+            )
+        {
+            Email = email;
+            Username = username;
+            Password = password;
+            IsAdmin = false;
+            IsBlocked = false;
+        }
+
+        public string Email { get; private set; }
+        public string Username { get; private set; }
+        public string Password { get; private set; }
+        public bool IsAdmin { get; set; }
+        public bool IsBlocked { get; set; }
+    }
+}
