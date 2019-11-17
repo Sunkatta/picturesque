08bc852ff7ed4c6c99a0081b2bf2f6a49a824e36
Lusian Manolov <lusianmanolov@outlook.com>
2019-11-17 18:03:09 +0200

    Added Common layer with CustomId and HashUtils classes

---
+++ PicturesqueAPI/Picturesque.Common/CustomObjects/CustomId.cs
@@ -0,0 +1,90 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Picturesque.Common
+{
+    public sealed class CustomId : IFormattable, IComparable, IComparable<CustomId>, IEquatable<CustomId>
+    {
+        private Guid id;
+
+        public CustomId()
+        {
+            this.id = Guid.NewGuid();
+        }
+
+        public CustomId(Guid guid)
+        {
+            this.id = guid;
+        }
+
+        public CustomId(string validationCode, bool extractFromValidationCode)
+        {
+            int substringLen = validationCode.IndexOf("--");
+            string guidAsString = validationCode.Substring(0, substringLen);
+            this.id = new Guid(guidAsString);
+        }
+
+        public int CompareTo(object obj)
+        {
+            return this.id.CompareTo(((CustomId)obj).id);
+        }
+
+        public int CompareTo(CustomId other)
+        {
+            return this.id.CompareTo(other.id);
+        }
+
+        public bool Equals(CustomId other)
+        {
+            return this.id.Equals(other.id);
+        }
+
+        public string ToString(string format, IFormatProvider formatProvider)
+        {
+            return this.id.ToString(format, formatProvider);
+        }
+
+        public static bool operator ==(CustomId item1, CustomId item2)
+        {
+            return item1.Equals(item2);
+        }
+
+        public static bool operator !=(CustomId item1, CustomId item2)
+        {
+            return !item1.Equals(item2);
+        }
+
+        public override bool Equals(object obj)
+        {
+            return (obj is CustomId) ? this.id.Equals(((CustomId)obj).id) : false;
+        }
+
+        public override int GetHashCode()
+        {
+            return this.id.GetHashCode();
+        }
+
+        public override string ToString()
+        {
+            return this.id.ToString();
+        }
+
+        public string ToString(string format)
+        {
+            return this.id.ToString(format);
+        }
+
+        public string Serialize()
+        {
+            return JsonConvert.SerializeObject(this,
+                 Formatting.None,
+                 new JsonSerializerSettings
+                 {
+                     NullValueHandling = NullValueHandling.Ignore,
+                     MissingMemberHandling = MissingMemberHandling.Ignore
+                 });
+        }
+    }
+}
---
+++ PicturesqueAPI/Picturesque.Common/Utils/HashUtils.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace Picturesque.Common
+{
+    public static class HashUtils
+    {
+        public static string CreateHashCode(string stringInput)
+        {
+            // STEP 1 Create the salt value with a cryptographic PRNG:
+            byte[] salt;
+            new RNGCryptoServiceProvider().GetBytes(salt = new byte[16]);
+
+            // STEP 2 Create the Rfc2898DeriveBytes and get the hash value:
+            // Note: Depending on the performance requirements of your specific application, the value '10000' can be reduced. A minimum value should be around 1000.
+            var pbkdf2 = new Rfc2898DeriveBytes(stringInput, salt, 10000);
+            byte[] hash = pbkdf2.GetBytes(20);
+
+            // STEP 3 Combine the salt and password bytes for later use:
+            byte[] hashBytes = new byte[36];
+            Array.Copy(salt, 0, hashBytes, 0, 16);
+            Array.Copy(hash, 0, hashBytes, 16, 20);
+
+            // STEP 4 Turn the combined salt+hash into a string for storage
+            string passwordHash = Convert.ToBase64String(hashBytes);
+
+            return passwordHash;
+        }
+    }
+}
