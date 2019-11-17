2fda20f04fc8d5450e689d825deff1566cfe8364
Lusian Manolov <lusianmanolov@outlook.com>
2019-11-17 19:29:08 +0200

    Added Login and Register components

---
+++ Picturesque/Picturesque/Components/Identity/LoginComponent.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Picturesque.Components
+{
+    public class LoginComponent : ComponentBase
+    {
+        public LoginInputModel loginInputModel = new LoginInputModel();
+
+        protected async Task HandleLogin()
+        {
+
+        }
+
+        public class LoginInputModel
+        {
+            [Required]
+            [EmailAddress]
+            public string Email { get; set; }
+
+            [Required]
+            [DataType(DataType.Password)]
+            public string Password { get; set; }
+        }
+    }
+}
---
+++ Picturesque/Picturesque/Components/Identity/RegisterComponent.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Picturesque.Components
+{
+    public class RegisterComponent : ComponentBase
+    {
+        public RegisterInputModel registerInputModel = new RegisterInputModel();
+
+        protected async Task HandleRegister()
+        {
+
+        }
+
+        public class RegisterInputModel
+        {
+            [Required]
+            [EmailAddress]
+            public string Email { get; set; }
+
+            [Required]
+            public string Username { get; set; }
+
+            [Required]
+            [DataType(DataType.Password)]
+            public string Password { get; set; }
+
+            [DataType(DataType.Password)]
+            [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
+            public string ConfirmPassword { get; set; }
+        }
+    }
+}
