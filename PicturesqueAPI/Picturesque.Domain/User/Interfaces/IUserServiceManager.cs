9dccd5964740c162359f43b7159cbca9e66799c8
Lusian Manolov <lusianmanolov@outlook.com>
2019-11-17 19:34:04 +0200

    Added IUserServiceManager

---
+++ PicturesqueAPI/Picturesque.Domain/User/Interfaces/IUserServiceManager.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Picturesque.Domain
+{
+    public interface IUserServiceManager
+    {
+        Task CreateUserAsync(User user);
+    }
+}
