cc90941e774ce16427fb53a0ded04a8fb5a984a9
Lusian Manolov <lusianmanolov@outlook.com>
2019-11-17 19:48:23 +0200

    Added AccountController and a Register method in it

---
+++ PicturesqueAPI/Picturesque.Application/DTOs/Account/RegisterUserEntry.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Picturesque.Application
+{
+    public sealed class RegisterUserEntry
+    {
+        [Required]
+        public string Username { get; set; }
+
+
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [MinLength(5)]
+        public string Password { get; set; }
+    }
+}
---
+++ PicturesqueAPI/PicturesqueAPI/Controllers/Identity/AccountController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Picturesque.Application;
+using Picturesque.DB;
+using Picturesque.Domain;
+using Picturesque.Services;
+
+namespace PicturesqueAPI.Controllers.Identity
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    [Produces("application/json")]
+    public class AccountController : Controller
+    {
+        private readonly IUserServiceManager _userManager;
+
+        public AccountController(PicturesqueDbContext ctx)
+        {
+            _userManager = new UserServiceManager(ctx);
+        }
+
+        [HttpPost("Register")]
+        public async Task<IActionResult> Register([FromBody] RegisterUserEntry entry)
+        {
+            try
+            {
+                User user =
+                    new User(
+                        entry.Email,
+                        entry.Username,
+                        entry.Password,
+                        false
+                        );
+
+                await _userManager.CreateUserAsync(user);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+
+            return Ok();
+        }
+    }
+}
