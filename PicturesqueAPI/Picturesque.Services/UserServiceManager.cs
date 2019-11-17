cde75c106a77728df6b8c301c23d5e2334cab7cf
Lusian Manolov <lusianmanolov@outlook.com>
2019-11-17 19:34:12 +0200

    Added UserServiceManager

---
+++ PicturesqueAPI/Picturesque.Services/UserServiceManager.cs
@@ -0,0 +1,25 @@
+﻿using Picturesque.DB;
+using Picturesque.Domain;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Picturesque.Services
+{
+    public sealed class UserServiceManager : IUserServiceManager
+    {
+        private readonly PicturesqueDbContext _ctx;
+
+        public UserServiceManager(PicturesqueDbContext ctx)
+        {
+            _ctx = ctx;
+        }
+
+        public async Task CreateUserAsync(User user)
+        {
+            await _ctx.Users.AddAsync(user);
+            await _ctx.SaveChangesAsync();
+        }
+    }
+}
