cec405f1ae02552ed518a1aa01aa2fa27c44e9ad
Lusian Manolov <lusianmanolov@outlook.com>
2019-11-18 19:57:52 +0200

    Added GameServiceManager and IGameServiceManager

---
+++ PicturesqueAPI/Picturesque.Domain/Game/Interfaces/IGameServiceManager.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Picturesque.Domain
+{
+    public interface IGameServiceManager
+    {
+        Task CreateGame();
+        Task GetGameOptions();
+    }
+}
---
+++ PicturesqueAPI/Picturesque.Services/GameServiceManager.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Picturesque.DB;
+using Picturesque.Domain;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Picturesque.Services
+{
+    public sealed class GameServiceManager : IGameServiceManager
+    {
+        private const int TIME_LIMIT = 10;
+        private const int NUMBER_OF_MISTAKES = 5;
+
+        private readonly PicturesqueDbContext _ctx;
+
+        public GameServiceManager(PicturesqueDbContext ctx)
+        {
+            _ctx = ctx;
+        }
+
+        public Task CreateGame()
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task GetGameOptions()
+        {
+            var categories = await _ctx.Categories.ToListAsync();
+            var difficulties = Enum.GetValues(typeof(Difficulty));
+        }
+    }
+}
