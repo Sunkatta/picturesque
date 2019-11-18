f35ac57ed073db3ca620bf71855fab2f9d31548f
Lusian Manolov <lusianmanolov@outlook.com>
2019-11-18 19:57:29 +0200

    Added Game model

---
+++ PicturesqueAPI/Picturesque.Domain/Game/Models/Game.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Picturesque.Domain
+{
+    public sealed class Game
+    {
+        public Game(
+            Category category,
+            Difficulty difficulty
+            )
+        {
+            Category = category;
+            Difficulty = difficulty;
+        }
+
+        public Category Category { get; set; }
+        public Difficulty Difficulty { get; set; }
+    }
+}
