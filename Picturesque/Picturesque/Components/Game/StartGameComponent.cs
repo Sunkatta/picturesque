0c2289ad2c7d084d64f25ab3ecb70e150ed23060
Lusian Manolov <lusianmanolov@outlook.com>
2019-11-18 21:56:03 +0200

    Added StartGameComponent

---
+++ Picturesque/Picturesque/Components/Game/StartGameComponent.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace Picturesque.Components
+{
+    public class StartGameComponent : ComponentBase
+    {
+        public GameOptions gameOptions;
+
+        protected override async Task OnInitializedAsync()
+        {
+            HttpClient client = new HttpClient();
+            gameOptions = await client.GetJsonAsync<GameOptions>("https://localhost:44317/api/Game/GetGameOptions");
+        }
+
+        protected async Task StartGame()
+        {
+
+        }
+
+        public class GameOptions
+        {
+            public string[] Categories { get; set; }
+            public string[] Difficulties { get; set; }
+        }
+    }
+}
