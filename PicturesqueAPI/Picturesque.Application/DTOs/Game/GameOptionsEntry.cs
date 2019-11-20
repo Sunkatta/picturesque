1bcf2016f3299fa0ea9d59eda94b9426fefa1a9f
Lusian Manolov <lusianmanolov@outlook.com>
2019-11-20 16:14:14 +0200

    Added GameOptionsEntry

---
+++ PicturesqueAPI/Picturesque.Application/DTOs/Game/GameOptionsEntry.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Picturesque.Application
+{
+    public sealed class GameOptionsEntry
+    {
+        public string CategoryId { get; set; }
+        public string Difficulty { get; set; }
+    }
+}
