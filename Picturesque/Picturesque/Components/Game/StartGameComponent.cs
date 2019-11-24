cf2d68e7a6ae003e3a01d6127f35bb0b2b7e27a5
Lusian Manolov <lusianmanolov@outlook.com>
2019-11-24 20:11:57 +0200

    Initial logic for guessing the pictures

--- Picturesque/Picturesque/Components/Game/StartGameComponent.cs
+++ Picturesque/Picturesque/Components/Game/StartGameComponent.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Components;
 using Picturesque.Models;
+using System;
 using System.Collections.Generic;
 using System.Net.Http;
 using System.Text.Json;
@@ -9,14 +10,17 @@ namespace Picturesque.Components
 {
     public class StartGameComponent : ComponentBase
     {
-        public Game game = new Game();
         public GameOptions gameOptions;
         public int numberOfTiles = 0;
+        public int numberOfPicturesVisible = 0;
+        public string successMessage = "";
+        public Picture selectedPicture = new Picture();
         public GameOptionsInputModel gameOptionsInputModel = new GameOptionsInputModel();
+        public Game game = new Game();
 
         protected override async Task OnInitializedAsync()
         {
-            game.Pictures = new List<string>();
+            game.Pictures = new List<Picture>();
 
             HttpClient client = new HttpClient();
             gameOptions = await client.GetJsonAsync<GameOptions>("https://localhost:44317/api/Game/GetGameOptions");
@@ -29,6 +33,30 @@ namespace Picturesque.Components
                     "https://localhost:44317/api/Game/StartGame",
                     gameOptionsInputModel
                     );
+        }
+
+        protected void ShowPicture(Picture picture)
+        {
+            numberOfPicturesVisible++;
+            picture.IsVisible = !picture.IsVisible;
+
+            if(selectedPicture != null && numberOfPicturesVisible == 2 && picture.IsVisible)
+            {
+                if(String.Compare(picture.Img2Base64.ToLower(), selectedPicture.Img2Base64.ToLower()) == 0)
+                {
+                    successMessage = "SUCCESS";
+                } else
+                {
+                    successMessage = "FAIL";
+                    selectedPicture.IsVisible = false;
+                    picture.IsVisible = false;
+                }
+                selectedPicture = null;
+                numberOfPicturesVisible = 0;
+            } else
+            {
+                selectedPicture = picture;
+            }
         }
     }
 }
