9b70f27f3e3a9d133af96fac8756a3b4fee3eafd
Lusian Manolov <lusianmanolov@outlook.com>
2019-11-24 16:01:44 +0200

    Added PicturesCategories table configuration

---
+++ PicturesqueAPI/Picturesque.DB/ModelBuilderConfigs/PicturesCategoriesConfig.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Picturesque.Domain;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Picturesque.DB.ModelBuilderConfigs
+{
+    internal sealed class PicturesCategoriesConfig : IEntityTypeConfiguration<PicturesCategories>
+    {
+        public void Configure(EntityTypeBuilder<PicturesCategories> builder)
+        {
+            builder
+                .HasKey(pc => new { pc.CategoryId, pc.PictureId });
+
+            builder
+                .HasOne(pc => pc.Category)
+                .WithMany(c => c.Pictures)
+                .HasForeignKey(uc => uc.CategoryId);
+
+            builder
+                .HasOne(pc => pc.Picture)
+                .WithMany(p => p.Categories)
+                .HasForeignKey(pc => pc.PictureId);
+        }
+    }
+}
