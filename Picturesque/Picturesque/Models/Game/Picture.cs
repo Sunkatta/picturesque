83a0f3e9f844e753b87a42c77478b0e55df2d0ab
Lusian Manolov <lusianmanolov@outlook.com>
2019-11-24 20:12:07 +0200

    Added Picture model

---
+++ Picturesque/Picturesque/Models/Game/Picture.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Picturesque.Models
+{
+    public class Picture
+    {
+        public string Img2Base64 { get; set; }
+        public bool IsVisible { get; set; }
+    }
+}
