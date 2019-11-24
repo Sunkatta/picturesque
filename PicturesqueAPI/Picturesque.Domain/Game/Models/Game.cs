cd917393deaa51808c314a56d37009a16f1f288b
Lusian Manolov <lusianmanolov@outlook.com>
2019-11-24 18:07:56 +0200

    Refactored Game and GameServiceManager

--- PicturesqueAPI/Picturesque.Domain/Game/Models/Game.cs
+++ PicturesqueAPI/Picturesque.Domain/Game/Models/Game.cs
@@ -14,32 +14,12 @@ namespace Picturesque.Domain
         {
             CategoryId = categoryId;
             Difficulty = difficulty;
-            NumberOfTiles = GetNumberOfTiles(Difficulty);
             Pictures = GetPictures(pictures);
         }
 
         public string CategoryId { get; private set; }
         public Difficulty Difficulty { get; private set; }
-        public int NumberOfTiles { get; private set; }
         public List<string> Pictures { get; set; }
-
-        private int GetNumberOfTiles(Difficulty difficulty)
-        {
-            int numberOfTiles = 0;
-
-            switch (difficulty)
-            {
-                case Difficulty.Easy: numberOfTiles = 16;
-                    break;
-                case Difficulty.Medium: numberOfTiles = 36;
-                    break;
-                case Difficulty.Hard: numberOfTiles = 64;
-                    break;
-                default: numberOfTiles = 0;
-                    break;
-            }
-            return numberOfTiles;
-        }
 
         private List<string> GetPictures(List<Picture> pictures)
         {
--- PicturesqueAPI/Picturesque.Services/GameServiceManager.cs
+++ PicturesqueAPI/Picturesque.Services/GameServiceManager.cs
@@ -14,6 +14,7 @@ namespace Picturesque.Services
     {
         private const int TIME_LIMIT = 10;
         private const int NUMBER_OF_MISTAKES = 5;
+        private static Random rand = new Random();
 
         private readonly PicturesqueDbContext _ctx;
 
@@ -24,18 +25,15 @@ namespace Picturesque.Services
 
         public async Task<Game> CreateGame(GameOptionsEntry gameOptions)
         {
-            Category category = 
+            Category category =
                 await _ctx.Categories
                 .FirstOrDefaultAsync(c => c.Id == gameOptions.CategoryId);
-
-            var pictures = 
-                await _ctx.Pictures
-                .Where(
-                    p => p.Categories.FirstOrDefault().CategoryId == category.Id)
-                .ToListAsync();
-
             Difficulty difficulty;
             Enum.TryParse(gameOptions.Difficulty, out difficulty);
+
+            int numberOfPictures = GetNumberOfPictures(difficulty);
+
+            List<Picture> pictures = await GetPictures(category, numberOfPictures);
 
             return new Game(category.Id, difficulty, pictures);
         }
@@ -47,5 +45,58 @@ namespace Picturesque.Services
 
             return new GameOptions(categories, difficulties);
         }
+
+        #region Private Methods
+
+        private static int GetNumberOfPictures(Difficulty difficulty)
+        {
+            int numberOfPictures = 0;
+
+            switch (difficulty)
+            {
+                case Difficulty.Easy:
+                    numberOfPictures = 8;
+                    break;
+                case Difficulty.Medium:
+                    numberOfPictures = 18;
+                    break;
+                case Difficulty.Hard:
+                    numberOfPictures = 32;
+                    break;
+                default:
+                    numberOfPictures = 0;
+                    break;
+            }
+
+            return numberOfPictures;
+        }
+
+        private async Task<List<Picture>> GetPictures(Category category, int numberOfPictures)
+        {
+            // Get the number of pictures
+            var pictures =
+                await _ctx.Pictures
+                .Where(
+                    p => p.Categories.FirstOrDefault().CategoryId == category.Id)
+                .Take(numberOfPictures)
+                .ToListAsync();
+
+            // Duplicate the pictures
+            pictures =
+                pictures
+                .SelectMany(p => Enumerable.Repeat(p, 2))
+                .ToList();
+
+            // Shuffle the list
+            pictures =
+                pictures
+                .Select(p => new { Value = p, Order = rand.Next() })
+                .OrderBy(x => x.Order)
+                .Select(p => p.Value)
+                .ToList();
+            return pictures;
+        }
+
+        #endregion
     }
 }
