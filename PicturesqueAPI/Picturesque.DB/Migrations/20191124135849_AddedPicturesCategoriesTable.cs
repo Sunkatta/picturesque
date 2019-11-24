79f4f08e4550a57635885801271fca388b69bab5
Lusian Manolov <lusianmanolov@outlook.com>
2019-11-24 16:01:53 +0200

    Added PicturesCategories table

---
+++ PicturesqueAPI/Picturesque.DB/Migrations/20191124135849_AddedPicturesCategoriesTable.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Picturesque.DB.Migrations
+{
+    public partial class AddedPicturesCategoriesTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "PicturesCategories",
+                columns: table => new
+                {
+                    CategoryId = table.Column<string>(nullable: false),
+                    PictureId = table.Column<string>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PicturesCategories", x => new { x.CategoryId, x.PictureId });
+                    table.ForeignKey(
+                        name: "FK_PicturesCategories_Categories_CategoryId",
+                        column: x => x.CategoryId,
+                        principalTable: "Categories",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_PicturesCategories_Pictures_PictureId",
+                        column: x => x.PictureId,
+                        principalTable: "Pictures",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PicturesCategories_PictureId",
+                table: "PicturesCategories",
+                column: "PictureId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "PicturesCategories");
+        }
+    }
+}
