5b42ef3b02e9eaa9a01bf885560f146e7b2276ea
Lusian Manolov <lmanolov@zingasoft.com>
2019-11-27 22:31:08 +0200

    Added Tests folder

---
+++ PicturesqueAPI/PicturesqueAPI.Tests/UnitTest1.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+
+namespace PicturesqueAPI.Tests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
