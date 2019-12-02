a4445d5e01108221b880dfd0c77c7877aa7fbc14
Lusian Manolov <lusianmanolov@outlook.com>
2019-12-02 21:01:22 +0200

    Added ICategoryServiceManager and CategoryServiceManager

---
+++ PicturesqueAPI/Picturesque.Domain/Game/Interfaces/ICategoryServiceManager.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Picturesque.Domain
+{
+    public interface ICategoryServiceManager
+    {
+        Task<IEnumerable<CategoryView>> GetCategoriesAsync();
+    }
+}
---
+++ PicturesqueAPI/Picturesque.Services/CategoryServiceManager.cs
@@ -0,0 +1,32 @@
+﻿using AutoMapper;
+using Microsoft.EntityFrameworkCore;
+using Picturesque.DB;
+using Picturesque.Domain;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Picturesque.Services
+{
+    public sealed class CategoryServiceManager : ICategoryServiceManager
+    {
+        private readonly PicturesqueDbContext _ctx;
+        private readonly IMapper _mapper;
+
+        public CategoryServiceManager(PicturesqueDbContext ctx, IMapper mapper)
+        {
+            _ctx = ctx;
+            _mapper = mapper;
+        }
+
+        public async Task<IEnumerable<CategoryView>> GetCategoriesAsync()
+        {
+            var rawCategories =
+                await _ctx.Categories.ToListAsync();
+            var categories = _mapper.Map<List<CategoryView>>(rawCategories);
+
+            return categories;
+        }
+    }
+}
