a8ed09eeaca50b7f5e4dfea06c0ab12bb0c4667b
Lusian Manolov <lusianmanolov@outlook.com>
2019-12-02 23:07:23 +0200

    Added Create and Delete Category DTOs

---
+++ PicturesqueAPI/Picturesque.Application/DTOs/Admin/CreateCategoryEntry.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Picturesque.Application
+{
+    public sealed class CreateCategoryEntry
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ PicturesqueAPI/Picturesque.Application/DTOs/Admin/DeleteCategoryEntry.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Picturesque.Application
+{
+    public sealed class DeleteCategoryEntry
+    {
+        public string Id { get; set; }
+    }
+}
