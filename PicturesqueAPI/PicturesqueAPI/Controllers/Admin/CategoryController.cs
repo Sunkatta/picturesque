c8a98f283ecb8d8e745b64439f73ee1af15daaba
Lusian Manolov <lusianmanolov@outlook.com>
2019-12-02 21:23:22 +0200

    Added CategoryController

---
+++ PicturesqueAPI/PicturesqueAPI/Controllers/Admin/CategoryController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Picturesque.Domain;
+
+namespace PicturesqueAPI.Controllers.Admin
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    [Produces("application/json")]
+    public class CategoryController : Controller
+    {
+        private readonly ICategoryServiceManager _categoryManager;
+
+        public CategoryController(ICategoryServiceManager categoryManager)
+        {
+            _categoryManager = categoryManager;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            return Ok(await _categoryManager.GetCategoriesAsync());
+        }
+    }
+}
