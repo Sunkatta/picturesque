71f615cf2dba7c1d9a97028da53d21bd7d4974fb
Lusian Manolov <lusianmanolov@outlook.com>
2019-12-03 23:50:02 +0200

    Added Update and Delete picture DTOs

---
+++ PicturesqueAPI/Picturesque.Application/DTOs/Admin/UpdatePictureEntry.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Picturesque.Application
+{
+    public sealed class UpdatePictureEntry
+    {
+        public string Id { get; set; }
+        public string CategoryId { get; set; }
+    }
+}
