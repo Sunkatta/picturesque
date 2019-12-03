faaad10f3bc0788e5e1b6ad8a059b8c7aae396a9
Lusian Manolov <lusianmanolov@outlook.com>
2019-12-03 20:51:31 +0200

    Extended Picture entity

--- Picturesque/Picturesque/Models/Game/Picture.cs
+++ Picturesque/Picturesque/Models/Game/Picture.cs
@@ -7,7 +7,10 @@ namespace Picturesque.Models
 {
     public class Picture
     {
+        public string Id { get; set; }
         public string Img2Base64 { get; set; }
         public bool IsVisible { get; set; }
+        public string CategoryName { get; set; }
+        public string CategoryId { get; set; }
     }
 }
