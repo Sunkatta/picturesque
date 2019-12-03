c6c52411e853a18f39a0c2c7917620b8e6e32145
Lusian Manolov <lusianmanolov@outlook.com>
2019-12-03 20:50:31 +0200

    Added Picture ViewModel

---
+++ PicturesqueAPI/Picturesque.Domain/Game/ViewModels/PictureView.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Picturesque.Domain
+{
+    public sealed class PictureView
+    {
+        public PictureView(
+            string id,
+            string img2base64,
+            string categoryName,
+            string categoryId
+        )
+        {
+            Id = id;
+            Img2Base64 = img2base64;
+            CategoryName = categoryName;
+            CategoryId = categoryId;
+        }
+
+        public string Id { get; set; }
+        public string Img2Base64 { get; set; }
+        public string CategoryName { get; set; }
+        public string CategoryId { get; set; }
+    }
+}
