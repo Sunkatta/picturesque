ae643db20878e9e5c104ba0895401a4143a25f79
Lusian Manolov <lusianmanolov@outlook.com>
2019-12-05 00:09:45 +0200

    Added GameScoreView

---
+++ PicturesqueAPI/Picturesque.Domain/Statistics/ViewModels/GameScoreView.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Picturesque.Domain
+{
+    public sealed class GameScoreView
+    {
+        public GameScoreView(
+            string username,
+            Difficulty difficulty,
+            string categoryName,
+            int score
+        )
+        {
+            Username = username;
+            Difficulty = difficulty;
+            CategoryName = categoryName;
+            Score = score;
+        }
+
+        public string Username { get; set; }
+        public Difficulty Difficulty { get; set; }
+        public string CategoryName { get; set; }
+        public int Score { get; set; }
+    }
+}
