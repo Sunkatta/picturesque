b2d3bc5df537cc4271b7b06fa25f83b8dd3fbe0e
Lusian Manolov <lusianmanolov@outlook.com>
2019-12-04 22:25:46 +0200

    Refinements in game logic and flow

--- Picturesque/Picturesque/Components/Game/StartGameComponent.cs
+++ Picturesque/Picturesque/Components/Game/StartGameComponent.cs
@@ -11,16 +11,25 @@ namespace Picturesque.Components
 {
     public class StartGameComponent : ComponentBase
     {
-        public GameOptions gameOptions;
-        public int numberOfTiles = 0;
+        public int numberOfSelectedPictures = 0;
         public int numberOfPicturesVisible = 0;
-        public string successMessage = "";
+        public int score = 0;
+        public int numberOfMistakesAllowed = 10;
+        public int counter;
+
+        public bool gameHasStarted = false;
+        public bool gameHasEnded = false;
+        public bool hasWon = false;
+        public bool hasLost = false;
+
+        public GameOptions gameOptions;
         public Picture selectedPicture = new Picture();
         public GameOptionsInputModel gameOptionsInputModel = new GameOptionsInputModel();
         public Game game = new Game();
 
         protected override async Task OnInitializedAsync()
         {
+            CleanUp();
             game.Pictures = new List<Picture>();
 
             HttpClient client = new HttpClient();
@@ -29,44 +38,124 @@ namespace Picturesque.Components
 
         protected async Task StartGame()
         {
+            CleanUp();
             HttpClient client = new HttpClient();
             game = await client.PostJsonAsync<Game>(
                     "https://localhost:44317/api/Game/StartGame",
                     gameOptionsInputModel
                     );
+
+            gameHasStarted = true;
+            SetCounter();
+            StartCountdown();
+        }
+
+        protected async Task SendScore()
+        {
+
         }
 
         protected async Task ShowPicture(Picture picture)
         {
-            if (!picture.IsVisible && numberOfPicturesVisible < 2)
+            if (numberOfMistakesAllowed != 0 && !gameHasEnded)
             {
-                numberOfPicturesVisible++;
-                picture.IsVisible = true;
-
-                if (selectedPicture != null && numberOfPicturesVisible == 2)
+                if (!picture.IsVisible && numberOfSelectedPictures < 2)
                 {
-                    await Task.Delay(500);
-                    if (String.Compare(picture.Img2Base64.ToLower(), selectedPicture.Img2Base64.ToLower()) == 0)
+                    numberOfSelectedPictures++;
+                    picture.IsVisible = true;
+
+                    if (selectedPicture != null && numberOfSelectedPictures == 2)
                     {
-                        successMessage = "SUCCESS";
+                        picture = await ComparePictures(picture);
                     }
                     else
                     {
-                        successMessage = "FAIL";
-                        selectedPicture.IsVisible = false;
-                        picture.IsVisible = false;
+                        selectedPicture = picture;
+                        numberOfSelectedPictures = 1;
                     }
-                    selectedPicture = null;
-                    picture = null;
-                    numberOfPicturesVisible = 0;
-                }
-                else
-                {
-                    selectedPicture = picture;
-                    numberOfPicturesVisible = 1;
                 }
             }
-            
+        }
+
+        private void SetCounter()
+        {
+            switch (game.Difficulty)
+            {
+                case 0: counter = 300;
+                    break;
+                case 1: counter = 600;
+                    break;
+                case 2: counter = 900;
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        private void StartCountdown()
+        {
+            var timer = new Timer(new TimerCallback(_ =>
+            {
+                if (counter <= 0 || numberOfMistakesAllowed == 0)
+                {
+                    gameHasEnded = true;
+                    hasLost = true;
+                    gameHasStarted = false;
+                    return;
+                }
+                counter--;
+
+                // Note that the following line is necessary because otherwise
+                // Blazor would not recognize the state change and not refresh the UI
+                this.StateHasChanged();
+            }), null, 1000, 1000);
+        }
+
+        private async Task<Picture> ComparePictures(Picture picture)
+        {
+            await Task.Delay(500);
+            if (String.Compare(picture.Img2Base64.ToLower(), selectedPicture.Img2Base64.ToLower()) == 0)
+            {
+                HandleSuccess();
+            }
+            else
+            {
+                HandleFail(picture);
+            }
+            selectedPicture = null;
+            picture = null;
+            numberOfSelectedPictures = 0;
+            return picture;
+        }
+
+        private void HandleSuccess()
+        {
+            score += 100 * numberOfMistakesAllowed;
+            numberOfPicturesVisible++;
+            if (numberOfPicturesVisible == game.Pictures.Count / 2)
+            {
+                hasWon = true;
+                gameHasEnded = true;
+                counter = 0;
+            }
+        }
+
+        private void HandleFail(Picture picture)
+        {
+            selectedPicture.IsVisible = false;
+            picture.IsVisible = false;
+            numberOfMistakesAllowed--;
+        }
+
+        private void CleanUp()
+        {
+            gameHasStarted = false;
+            gameHasEnded = false;
+            hasWon = false;
+            hasLost = false;
+            score = 0;
+            numberOfMistakesAllowed = 10;
+            numberOfPicturesVisible = 0;
         }
     }
 }
