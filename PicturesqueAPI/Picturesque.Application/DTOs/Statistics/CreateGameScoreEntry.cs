ec28696f2c823a25afa767ba995bba7dc137af93
Lusian Manolov <lusianmanolov@outlook.com>
2019-12-04 23:32:13 +0200

    Added CreateGameScore DTO

---
+++ PicturesqueAPI/Picturesque.Application/DTOs/Statistics/CreateGameScoreEntry.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Picturesque.Application
+{
+    public sealed class CreateGameScoreEntry
+    {
+        public string UserId { get; set; }
+        public string CategoryId { get; set; }
+        public int Difficulty { get; set; }
+        public int Score { get; set; }
+    }
+}
