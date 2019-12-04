a5ce192f72e20d1bd034dfb3d6bc0373de62b29b
Lusian Manolov <lusianmanolov@outlook.com>
2019-12-04 23:32:26 +0200

    Extended LoginUserEntry

--- PicturesqueAPI/Picturesque.Application/DTOs/Account/LoginUserEntry.cs
+++ PicturesqueAPI/Picturesque.Application/DTOs/Account/LoginUserEntry.cs
@@ -6,6 +6,7 @@ namespace Picturesque.Application
 {
     public sealed class LoginUserEntry
     {
+        public string Id { get; set; }
         public string Email { get; set; }
         public string Password { get; set; }
         public bool IsAdmin { get; set; }
