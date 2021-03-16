efb1e2a6931b9b5060830e5dbc9912d01f21c060
Lusian Manolov <lusianmanolov@outlook.com>
2021-03-16 19:19:19 +0200

    Added Confirm Email page and component

---
+++ Picturesque/Picturesque/Components/Identity/ConfirmEmailComponent.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Picturesque.Components
+{
+    public class ConfirmEmailComponent : ComponentBase
+    {
+        [Parameter]
+        public string Email { get; set; }
+
+        [Parameter]
+        public string ConfirmationCode { get; set; }
+    }
+}
