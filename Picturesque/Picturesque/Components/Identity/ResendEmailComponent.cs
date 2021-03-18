e81d9a37e7d0099b9bb9b9900e2ae2536822f8dd
Lusian Manolov <lusianmanolov@outlook.com>
2021-03-18 21:46:48 +0200

    Added page for resending account confirmation email

---
+++ Picturesque/Picturesque/Components/Identity/ResendEmailComponent.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Components;
+using Picturesque.Models;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace Picturesque.Components
+{
+    public class ResendEmailComponent : ComponentBase
+    {
+        public EmailInputModel emailInputModel = new EmailInputModel();
+        public bool hasEmailBeenSent = false;
+
+        public async Task HandleResendEmail()
+        {
+            HttpClient client = new HttpClient();
+            await client.PostJsonAsync("https://localhost:44317/api/Account/ResendConfirmationEmail", emailInputModel);
+            hasEmailBeenSent = true;
+        }
+    }
+}
---
+++ Picturesque/Picturesque/Models/Identity/EmailInputModel.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Picturesque.Models
+{
+    public class EmailInputModel
+    {
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+    }
+}
