42d80d962f116e4cb87c2a757ad0650d801b65b4
Lusian Manolov <lusianmanolov@outlook.com>
2021-03-21 16:27:31 +0200

    Added Reset Password page

---
+++ Picturesque/Picturesque/Models/Identity/ResetPasswordInputModel.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Picturesque.Models
+{
+    public class ResetPasswordInputModel
+    {
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [MinLength(5, ErrorMessage = "Password must be at least 5 characters.")]
+        public string Password { get; set; }
+
+        [DataType(DataType.Password)]
+        [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
+        public string ConfirmPassword { get; set; }
+
+        public string PasswordResetCode { get; set; }
+    }
+}
