0713112a8e37d92ca4e684dec4a16730231914db
Lusian Manolov <lusianmanolov@outlook.com>
2021-03-21 16:27:22 +0200

    Added Forgot Password page

---
+++ Picturesque/Picturesque/Components/Identity/ForgotPasswordComponent.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Components;
+using Picturesque.Models;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace Picturesque.Components
+{
+    public class ForgotPasswordComponent : ComponentBase
+    {
+        public EmailInputModel emailInputModel = new EmailInputModel();
+        public bool hasEmailBeenSent = false;
+
+        public async Task HandleForgotPassword()
+        {
+            HttpClient client = new HttpClient();
+            await client.PostJsonAsync("https://localhost:44317/api/Account/ForgotPassword", emailInputModel);
+            hasEmailBeenSent = true;
+        }
+    }
+}
