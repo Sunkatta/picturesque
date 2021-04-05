88f3620c0c36b02f0eb7777968df2eda63b6cdca
Lusian Manolov <lusianmanolov@outlook.com>
2021-04-05 20:46:07 +0300

    Tweaked password configuration

--- PicturesqueAPI/PicturesqueAPI/Startup.cs
+++ PicturesqueAPI/PicturesqueAPI/Startup.cs
@@ -34,7 +34,13 @@ namespace PicturesqueAPI
                 options.UseSqlServer(
                     Configuration.GetConnectionString("dbconn")));
 
-            services.AddIdentity<User, IdentityRole>()
+            services.AddIdentity<User, IdentityRole>(options =>
+            {
+                options.Password.RequireDigit = false;
+                options.Password.RequireNonAlphanumeric = false;
+                options.Password.RequireLowercase = false;
+                options.Password.RequireUppercase = false;
+            })
                 .AddEntityFrameworkStores<PicturesqueDbContext>()
                 .AddTokenProvider<DataProtectorTokenProvider<User>>(TokenOptions.DefaultProvider);
 
