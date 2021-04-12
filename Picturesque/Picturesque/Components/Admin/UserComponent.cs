86f8297186d6adc96f0722a66d4e392299ea950d
Lusian Manolov <lusianmanolov@outlook.com>
2021-04-12 21:31:37 +0300

    Added authorization headers when blocking user

--- Picturesque/Picturesque/Components/Admin/UserComponent.cs
+++ Picturesque/Picturesque/Components/Admin/UserComponent.cs
@@ -4,18 +4,21 @@ using Picturesque.Models.Constants;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http;
+using System.Net.Http.Headers;
 
 namespace Picturesque.Components
 {
     public class UserComponent : ComponentBase
     {
         public int counter = 1;
+        public string token;
 
         public List<User> users;
 
         public async void BlockUser(string id)
         {
             HttpClient client = new HttpClient();
+            client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
             bool isBlocked = await client.PostJsonAsync<bool>(ApiConstants.ApiUrl + "User/Block", id);
             User user = users.FirstOrDefault(u => u.Id == id);
             user.IsBlocked = isBlocked;
