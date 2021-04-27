b43b00cd0a8bfa4bc819c4f97456686049591ecf
Lusian Manolov <lusianmanolov@outlook.com>
2021-04-27 20:22:49 +0300

    Added Leaderboard component

---
+++ Picturesque/Picturesque/Components/Game/LeaderboardComponent.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Components;
+using Picturesque.Models;
+using Picturesque.Models.Constants;
+using Picturesque.Models.Enums;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+
+namespace Picturesque.Components
+{
+    public class LeaderboardComponent : ComponentBase
+    {
+        public GameScore[] gameScores;
+        public Difficulty selectedDifficulty;
+        public string token;
+
+        public async Task GetLeaderboard()
+        {
+            HttpClient client = new HttpClient();
+            client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+            gameScores = await client.GetJsonAsync<GameScore[]>(ApiConstants.ApiUrl + "Statistics");
+            selectedDifficulty = Difficulty.All;
+        }
+    }
+}
