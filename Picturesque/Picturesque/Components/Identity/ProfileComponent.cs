a763f8cd371c7f8212fee40f34a7cd842196ce5d
Lusian Manolov <lusianmanolov@outlook.com>
2021-05-03 18:58:58 +0300

    Added Profile Component and routing to it

---
+++ Picturesque/Picturesque/Components/Identity/ProfileComponent.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Picturesque.Components
+{
+    public class ProfileComponent : ComponentBase
+    {
+        [Parameter]
+        public int TabIndex { get; set; }
+
+        protected int tabIndex = 0;
+
+        protected override void OnAfterRender(bool firstRender)
+        {
+            if (firstRender)
+            {
+                tabIndex = TabIndex;
+                StateHasChanged();
+            }
+        }
+    }
+}
