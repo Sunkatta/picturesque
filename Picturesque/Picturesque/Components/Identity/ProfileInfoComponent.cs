4d184add2336465228df3d1c934f21d53feebb0b
Lusian Manolov <lusianmanolov@outlook.com>
2021-05-04 18:06:58 +0300

    Added Profile Info component

---
+++ Picturesque/Picturesque/Components/Identity/ProfileInfoComponent.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Components;
+using Picturesque.Models;
+using Picturesque.Models.Constants;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+
+namespace Picturesque.Components
+{
+    public class ProfileInfoComponent : ComponentBase
+    {
+        protected string token;
+        protected Profile profile;
+        protected HttpClient httpClient;
+
+        protected async Task GetProfile(string id)
+        {
+            httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+            profile = await httpClient.GetJsonAsync<Profile>(ApiConstants.ApiUrl + "Account/Profile/" + id);
+        }
+    }
+}
---
+++ Picturesque/Picturesque/Models/Identity/Profile.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Picturesque.Models
+{
+    public class Profile
+    {
+        public string Email { get; set; }
+
+        public string Username { get; set; }
+
+        public DateTime CreatedOn { get; set; }
+
+        public string ProfilePic { get; set; }
+    }
+}
