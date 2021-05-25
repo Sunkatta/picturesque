d16fbb1d3eec7aa1c8873e755593d84177497bfc
Lusian Manolov <lusianmanolov@outlook.com>
2021-05-25 20:39:02 +0300

    Extended User model

--- PicturesqueAPI/Picturesque.Domain/User/Models/User.cs
+++ PicturesqueAPI/Picturesque.Domain/User/Models/User.cs
@@ -5,6 +5,10 @@ namespace Picturesque.Domain
 {
     public sealed class User : IdentityUser
     {
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
         public bool IsAdmin { get; set; }
 
         public bool IsBlocked { get; set; }
